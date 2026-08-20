1dc9905453b57994cd536cd68d3a270df81a25fa
koffykraft <info@koffykraft.com>
2026-08-20 14:08:04 +0530

    WO-0012 add AWS runtime package

---
+++ src/aws_runtime/__init__.py
@@ -0,0 +1,5 @@
+"""AWS deployment adapters for TAGRO ECHO OS.
+
+This package is infrastructure-facing. Business truth remains in the domain/runtime
+modules and repository contracts under src/.
+"""
